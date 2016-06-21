a152e250732301b3b90a9638943861cddec10c0a
JzJad <jimmy.jad@gmail.com>
2016-06-21 08:24:12 -0500

    Add project files.

---
+++ MTR-Bot/Controllers/MessagesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Connector.Utilities;
+using Newtonsoft.Json;
+
+namespace MTR_Bot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<Message> Post([FromBody]Message message)
+        {
+            if (message.Type == "Message")
+            {
+                return message.CreateReplyMessage($"What symtoms is your computer having?");
+            }
+            else
+            {
+                return HandleSystemMessage(message);
+                //return message.CreateReplyMessage($"I'm sorry I didnt quite understand that. Please note that I am currently in an early alpha phase and my responses and understanding are very limited");
+            }
+        }
+
+        private Message HandleSystemMessage(Message message)
+        {
+            if (message.Type == "Ping")
+            {
+                Message reply = message.CreateReplyMessage($"Pong!");
+                reply.Type = "Ping";
+                return reply;
+            }
+            else if (message.Type == "DeleteUserData")
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == "BotAddedToConversation")
+            {
+            }
+            else if (message.Type == "BotRemovedFromConversation")
+            {
+            }
+            else if (message.Type == "UserAddedToConversation")
+            {
+                Message reply = message.CreateReplyMessage($"Hello! {}");
+                reply.Type = "UserAddedToConversation";
+                return reply;
+            }
+            else if (message.Type == "UserRemovedFromConversation")
+            {
+            }
+            else if (message.Type == "EndOfConversation")
+            {
+            }
+
+            return null;
+        }
+    }
+}
